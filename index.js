e03d6510b8a0a231bf8a3b10959fa0fc45c29034
thenevermore <nurhadiarian@gmail.com>
2022-06-07 14:24:25 +0700

    Done tugas

--- index.js
+++ index.js
@@ -6,20 +6,32 @@ const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
 // "1. Nama"
 // "2. Nama"
 // ...
-const sorter = null;
+const sorter = (array, method) => {
+  let resultMethod = method(array);
+  let result = [];
+  for (let i = 0; i < resultMethod.length; i++) {
+    result.push(`${i + 1}. ${resultMethod[i]}`);
+  }
+
+  return result;
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara ascending
 // mengembalikan array yang sudah diurutkan
-const sortAscending = null;
+const sortAscending = (array) => {
+  return array.sort()
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara descending
 // mengembalikan array yang sudah diurutkan
-const sortDescending = null;
+const sortDescending = (array) => {
+  return array.reverse()
+};
 
 // ! JANGAN DIMODIFIKASI
 (function main() {
-  console.log(sorter?.(names, sortAscending)?.join("\n"));
-  console.log(sorter?.(names, sortDescending)?.join("\n"));
+  console.log(sorter ?.(names, sortAscending) ?.join("\n"));
+  console.log(sorter ?.(names, sortDescending) ?.join("\n"));
 })();
 
 module.exports = {
